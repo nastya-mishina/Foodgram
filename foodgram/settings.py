9922e85f396bdd911b1d306afa7eb71b79e6a438
Анастасия <anastasia.petrukhova@gmail.com>
2021-05-24 02:06:13 +0300

    thrid review

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.154.214.43']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
