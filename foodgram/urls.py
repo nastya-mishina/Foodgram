d35462cf890f3addfd15442b3fe0244ed9eb18cf
Анастасия <anastasia.petrukhova@gmail.com>
2021-05-26 01:15:59 +0300

    third review

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -12,7 +12,8 @@ flatpages_urls = [
     path('flatpages/', include('django.contrib.flatpages.urls')),
     path('author/', views.flatpage, {'url': '/author/'}, name='about_author'),
     path('tech/', views.flatpage, {'url': '/tech/'}, name='about_tech'),
-    path('site-info/', views.flatpage, {'url': '/site-info/'}, name='site_info'),
+    path('site-info/', views.flatpage, {'url': '/site-info/'},
+         name='site_info'),
 ]
 
 urlpatterns = [
